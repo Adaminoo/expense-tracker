85463f239830153ae5be1dd6c18f678217a22e73
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2025-03-20 08:01:09 -0600

    test

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function fart() {
+  window.alert("braaaaaaaaap hahahahahahaha")
+}
+
+function MyButton() {
+  return (
+    <button onClick={fart}>
+      fart
+    </button>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton></MyButton>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
